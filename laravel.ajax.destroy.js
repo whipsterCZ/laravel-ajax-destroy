e2a3166af432c94482416513f855d3b340120bf3
Daniel Kouba <dan@brainz.cz>
2016-05-31 00:29:31 +0200

    UMD - unified module definiton

--- laravel.ajax.destroy.js
+++ laravel.ajax.destroy.js
@@ -1,4 +1,22 @@
-var laravel = (function($, laravel){
+/**
+ * Laravel AJAX Destroy - calling REST Destroy method without need of Form::submit()
+ *
+ * @dependency jQuery >2
+ * @dependency laravel.ajax.js https://github.com/whipsterCZ/laravel-ajax
+ * @author Ing. Daniel Kouba <me@danielkouba.cz>
+ */
+
+var factory = function(){
+    var $ = window.jQuery;
+    if (typeof $ !== 'function') {
+        return console.error('laravel.ajax.destroy.js: jQuery is missing, load it please');
+    }
+    if(typeof window.laravel !== 'object') {
+        return console.error('laravel.ajax.destroy.destroy.js: laravel is missing, load it please')
+    }
+
+    var laravel = window.laravel;
+
     laravel.destroy = {};
 
     laravel.destroy.init = function(){
@@ -28,5 +46,18 @@ var laravel = (function($, laravel){
     };
 
     laravel.destroy.init();
+
     return laravel;
-})(jQuery, laravel || {});
+
+};
+
+
+//UMD - unified module definition
+(function(name,context,factory) {
+    if (typeof module != 'undefined' && module.exports)
+        module.exports = factory();
+    else if (typeof define == 'function' && define.amd)
+        define(name, factory);
+    else
+        context[name] = factory();
+}('laravel',this, factory));
